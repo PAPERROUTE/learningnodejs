ee800f792df8e235ba8170380ff48bfd08a73d8b
Jake <jakegeo@sudomail.com>
2022-04-30 11:38:27 -0700

    learning to write to a file using node

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
+const fileIn = fs.readFileSync('file.txt', 'utf-8');
+console.log(fileIn);
 
-const hello = 'Hello world!';
-console.log(hello);
+
+
+const textOut = `This is me writing to  the file: ${fileIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('output.txt', textOut);
+console.log("File written!");
